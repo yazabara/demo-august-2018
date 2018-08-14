ec12d395e67863baa6cc22ed8b9b8999cb6d1b7b
Yaroslav Zabara <yaroslav.zabara@waveaccess.ru>
2018-08-14 11:07:49 +0300

    - PrincipalExtractor added: logged in users are stored in db.

--- src/main/java/com/yazabara/demoaugust2018/config/security/WebSecurityConfig.java
+++ src/main/java/com/yazabara/demoaugust2018/config/security/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package com.yazabara.demoaugust2018.config.security;
 
+import com.yazabara.demoaugust2018.model.db.DbUser;
+import com.yazabara.demoaugust2018.repo.UserRepository;
 import com.yazabara.demoaugust2018.service.security.WebUserDetailsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Sso
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final WebUserDetailsService userDetailsService;
@@ -51,4 +59,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return authProvider;
     }
 
+    @Bean
+    public PrincipalExtractor principalExtractor(UserRepository userRepository) {
+        return map -> {
+            String login = (String) map.get("login");
+            Optional<DbUser> byUsername = userRepository.findByUsername(login);
+            DbUser user;
+            if (!byUsername.isPresent()) {
+                log.info("No user found, generating profile for {}", login);
+                //TODO generate temp password
+                user = userRepository.save(new DbUser().withUsername(login).withPassword(login));
+            } else {
+                log.info("User found: {}", byUsername.get());
+                user = byUsername.get();
+            }
+            final User.UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
+            return userBuilder
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRole())
+                    .build();
+        };
+    }
 }
